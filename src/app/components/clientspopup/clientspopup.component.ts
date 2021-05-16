2f8e983e10f793c86f0397455804ad5867a2cf68
anilkumar2121 <anilcruel2121@gmail.com>
2021-05-16 22:05:08 +0530

    clients

--- src/app/components/clientspopup/clientspopup.component.ts
+++ src/app/components/clientspopup/clientspopup.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { MatDialog, MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
+
 
 @Component({
   selector: 'app-clientspopup',
@@ -7,9 +9,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ClientspopupComponent implements OnInit {
 
-  constructor() { }
+  constructor(private dialogRef:MatDialogRef<ClientspopupComponent>) { }
 
   ngOnInit(): void {
+  }
+
+  closeModal(){
+    this.dialogRef.close();
   }
 
   
